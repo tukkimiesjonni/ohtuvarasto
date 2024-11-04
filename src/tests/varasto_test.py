54949a479c9a663a281b03593e35ad9c6a0d7c73
Tukkimies Jonni E <jonnituk@cubbli22-gold.local>
2024-11-04 18:50:08 +0200

    Added better coverage for Varasto class tests

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,55 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_laittaa_liikaa(self):
+        self.varasto.lisaa_varastoon(11)
+
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 0)
+
+    def test_ottaa_liikaa(self):
+        self.varasto.lisaa_varastoon(10)
+        self.varasto.ota_varastosta(11)
+
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10)
+
+    def test_paljonko_mahtuu(self):
+        self.varasto.lisaa_varastoon(2)
+
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 8)
+
+    def test_ei_voi_lisata_negatiivista(self):
+        self.varasto.lisaa_varastoon(-2)
+
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10)
+
+    def test_ei_voi_vahentaa_negatiivista(self):
+        self.varasto.lisaa_varastoon(4)
+        self.varasto.ota_varastosta(-2)
+
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 6)
+
+    def test_string_method(self):
+        
+        self.assertEqual(str(self.varasto), "saldo = 0, vielä tilaa 10")
+
+    def test_lisays_nollalla(self):
+        self.varasto.lisaa_varastoon(0)
+
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10)
+
+    def test_otto_nollalla(self):
+        self.varasto.lisaa_varastoon(3)
+        self.varasto.ota_varastosta(0)
+
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 7)
+
+    def test_negatiivinen_tilavuus(self):
+        self.varasto = Varasto(-10)
+
+        self.assertAlmostEqual(self.varasto.tilavuus, 0)
+
+    def test_negatiivinen_alkusaldo(self):
+        self.varasto = Varasto(10, -2)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
